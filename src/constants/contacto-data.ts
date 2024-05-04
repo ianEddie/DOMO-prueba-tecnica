c67703cddaa0faf97108234a2230482ba81970b4
ian eddie <ianeddie2@gmail.com>
2024-05-04 18:11:18 -0300

    Responsive design

---
+++ src/constants/contacto-data.ts
@@ -0,0 +1,33 @@
+import GmailIcon from '@/icons/gmail-icon.astro'
+import LinkeinIcon from '@/icons/linkedin-icon.astro'
+import TelephoneIcon from '@/icons/telephone-icon.astro'
+import WhatsappIcon from '@/icons/whatsapp-icon.astro'
+import WebIcon from '@/icons/web-icon.astro'
+
+export const CONTACTO = [
+  {
+    icon: GmailIcon,
+    value: 'mailto:ianeddie2@gmail.com?Subject=DOMO%prueba%tecnica',
+    name: 'ianeddie2@gmail.com'
+  },
+  {
+    icon: LinkeinIcon,
+    value: 'https://www.linkedin.com/in/ian-nicolas-ortiz',
+    name: 'Linkedin'
+  },
+  {
+    icon: TelephoneIcon,
+    value: 'tel:+54 1173615762',
+    name: 'Telefono'
+  },
+  {
+    icon: WhatsappIcon,
+    value: 'https://wa.me/541173615762',
+    name: 'Whatsapp'
+  },
+  {
+    icon: WebIcon,
+    value: 'https://ian-ortiz-developer.vercel.app/',
+    name: 'Portfolio'
+  }
+]
---
+++ src/constants/header-data.ts
@@ -0,0 +1,32 @@
+export const HEADER_DATA = [
+  {
+    name: 'Home',
+    value: '#home',
+    color: 'var(--text-color-primary)'
+  },
+  {
+    name: 'Services',
+    value: '#services',
+    color: 'var(--text-color-secondary)'
+  },
+  {
+    name: 'Technology',
+    value: '#technology',
+    color: 'var(--text-color-secondary)'
+  },
+  {
+    name: 'Case studies',
+    value: '#footer',
+    color: 'var(--text-color-secondary)'
+  },
+  {
+    name: 'About',
+    value: '#clients',
+    color: 'var(--text-color-secondary)'
+  },
+  {
+    name: 'Hire us',
+    value: '#footer',
+    color: 'var(--text-color-primary)'
+  }
+]
